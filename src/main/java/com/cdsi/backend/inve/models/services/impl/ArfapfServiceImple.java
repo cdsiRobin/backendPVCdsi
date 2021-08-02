c1d35ae5dccfdda065a1fa7e717cce322a0d96be
Robin-Casa <sant4n4.1@gmail.com>
2021-08-02 18:17:18 -0500

    Creacion de la entidad Arfafp(tabla de forma de pago) con su controlador

---
+++ src/main/java/com/cdsi/backend/inve/models/services/impl/ArfapfServiceImple.java
@@ -0,0 +1,66 @@
+package com.cdsi.backend.inve.models.services.impl;
+
+import com.cdsi.backend.inve.models.dao.IArfafpRepo;
+import com.cdsi.backend.inve.models.entity.Arfafp;
+import com.cdsi.backend.inve.models.entity.ArfafpPK;
+import com.cdsi.backend.inve.models.services.IArfafpService;
+import com.cdsi.backend.inve.models.services.exception.ServiceException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class ArfapfServiceImple implements IArfafpService {
+
+    @Autowired
+    private IArfafpRepo iArfafpRepo;
+
+    @Override
+    public List<Arfafp> findByCiaAndEstado(String cia, String estado) throws ServiceException {
+        try {
+            List<Arfafp> arfafpList = this.iArfafpRepo.listarCiaAndEstado(cia,estado);
+            return arfafpList;
+        }catch (Exception e){
+            log.error(e.getMessage());
+        }
+        return null;
+    }
+
+    @Override
+    public Arfafp buscarId(ArfafpPK arfafpPK) throws ServiceException {
+        try {
+            return this.iArfafpRepo.buscarId(arfafpPK.getNoCia(), arfafpPK.getTipoFpago(), arfafpPK.getCodFpago());
+        }catch (Exception e){
+            log.error(e.getMessage());
+        }
+        return null;
+    }
+
+    @Override
+    public Arfafp findById(Long id) throws ServiceException {
+        return null;
+    }
+
+    @Override
+    public List<Arfafp> getAll() throws ServiceException {
+        return null;
+    }
+
+    @Override
+    public List<Arfafp> findByLike(Arfafp arfafp) throws ServiceException {
+        return null;
+    }
+
+    @Override
+    public Arfafp save(Arfafp arfafp) throws ServiceException {
+        return null;
+    }
+
+    @Override
+    public Arfafp delete(Long id) throws ServiceException {
+        return null;
+    }
+}
