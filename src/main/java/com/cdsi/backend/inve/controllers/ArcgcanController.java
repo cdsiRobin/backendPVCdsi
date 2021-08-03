227dcf83a2328a732a4aaa7f9d3597e363610a8a
Robin-Casa <sant4n4.1@gmail.com>
2021-08-03 18:25:34 -0500

    Creando el controlador de la entidada Arcgcan

---
+++ src/main/java/com/cdsi/backend/inve/controllers/ArcgcanController.java
@@ -0,0 +1,61 @@
+package com.cdsi.backend.inve.controllers;
+
+
+import com.cdsi.backend.inve.controllers.commons.ResponseRest;
+import com.cdsi.backend.inve.controllers.generic.GenericController;
+import com.cdsi.backend.inve.models.entity.Arcgcan;
+import com.cdsi.backend.inve.models.entity.ArcgcanPK;
+import com.cdsi.backend.inve.models.services.IArcgcanService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/trans")
+public class ArcgcanController extends GenericController {
+    @Autowired
+    private IArcgcanService iArcgcanService;
+
+    //METODO QUE NOS PERMITE BUSCAR UN CENTRO DE COSTO
+    @PostMapping("/id")
+    public ResponseEntity<ResponseRest> buscarId(@RequestBody ArcgcanPK arcgcanPK, BindingResult result) {
+        if (result.hasErrors()){
+            return super.getErrorRequest();
+        }
+        try{
+            Object obj = this.iArcgcanService.buscarId(arcgcanPK);
+            if (obj != null){
+                return super.getOKConsultaRequest(obj);
+            }
+            return super.getBadIdRequest();
+        }catch (Exception e){
+            log.error(e.getMessage());
+            return super.getBadRequest(e.getMessage());
+        }
+    }
+
+    //VAMOS A TRAER TODOS LOS CENTROS DE COSTO
+    @PostMapping("/listar")
+    public ResponseEntity<ResponseRest> listar(@RequestBody Arcgcan arcgcan, BindingResult result){
+        if (result.hasErrors()){
+            return super.getErrorRequest();
+        }
+        try{
+            Object obj = this.iArcgcanService.listarByCiaAndEstado(arcgcan);
+            if (obj != null){
+                return super.getOKConsultaRequest(obj);
+            }
+            return super.getBadIdRequest();
+        }catch (Exception e){
+            log.error(e.getMessage());
+            return super.getBadRequest(e.getMessage());
+        }
+    }
+
+}
