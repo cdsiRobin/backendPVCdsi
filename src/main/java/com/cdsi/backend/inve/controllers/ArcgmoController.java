4b775f0b3d328edd738535ca0b678392f111506b
robin <rsantanall@cdsi.com.pe>
2021-11-29 19:06:51 -0500

    Creando el REst de monedas(Arcgmo), que lista todas las monedas

---
+++ src/main/java/com/cdsi/backend/inve/controllers/ArcgmoController.java
@@ -0,0 +1,32 @@
+package com.cdsi.backend.inve.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cdsi.backend.inve.controllers.commons.ResponseRest;
+import com.cdsi.backend.inve.controllers.generic.GenericController;
+import com.cdsi.backend.inve.models.services.IArcgmoService;
+
+@RestController
+@RequestMapping("/api/arcgmo")
+public class ArcgmoController extends GenericController {
+  @Autowired
+  private IArcgmoService arcgServi;
+  
+  @GetMapping
+  public ResponseEntity<ResponseRest> listar(){
+      try{
+          Object obj = this.arcgServi.listarMonedas();
+          if (obj != null){
+              return super.getOKConsultaRequest(obj);
+          }
+          return super.getBadIdRequest();
+      }catch (Exception e){
+          return super.getBadRequest(e.getMessage());
+      }
+  }
+  
+}
